3600e6252a5e1737773bb0c15fc20d12f485ce90
Pierre Colombo <pierrecolombo@mbp-de-pierre.home>
2017-05-04 20:24:15 +0200

    algo working and tested

--- src/main.m
+++ src/main.m
@@ -1,20 +1,26 @@
-% Master Script For Matrix Factorisation
+% Master Script For Matrix Factorisation : This script factor a matrix A
+% of size N*N into a product of two matrices U.U^T
 
 % Constant
-N = 3; % Size of the matrix
-accuracy = 0.001; % accuracy you would like to reach
-stepSize = accuracy/4; % for the gradient descent
+N = 2; % Size of the matrix
+accuracy = 0.01; % accuracy you would like to reach
+stepSize = accuracy; % for the gradient descent
+epsilon = accuracy + 1;
 
 % Initialization
 X = zeros(N,N);
-epsilon = accuracy + 1;
-
 
 % Generate a symetric matrix to factor
 d = 10*rand(N,1); % The diagonal values
 t = triu(bsxfun(@min,d,d.').*rand(N),1); % The upper trianglar random values
 A = diag(d)+t+t.'; % Put them together in a symmetric matrix
 
+stringA = 'matrix A';
+disp(stringA)
+disp(A);
+
+
+% Compute X :
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 %%%% Gradient descent for the programm %%%%%
@@ -24,9 +30,43 @@ A = diag(d)+t+t.'; % Put them together in a symmetric matrix
 number_of_projections = 0; % number of projections ( O(1/accuracy^2) )
 while epsilon > accuracy  
     gradient = compute_KL_gradient(A,X);
-    X = X - stepSize * gradient;
+    X = X - stepSize* gradient;
     X = find_closest_symetric_matrix(X);
-    epsilon = norm(exp(X)-A,'fro');
+    epsilon = norm(gradient,'fro');
     number_of_projections = number_of_projections +1;
+    
+    stringIteration = 'number of iterations';
+    disp(stringIteration);
+    disp(number_of_projections);
+
 end
+
+% compute exp_H(X)
+ 
+% just apply piece wise function
+E_HX = exp(X);
+
+% compute U 
+
+% fix the level of approximation : 
+% as the rank is 2 : we can use k = 2^2
+k = 10; 
+
+% compute U 
+U = perform_factorization_expH(X, k); 
+
+stringU = 'matrix U';
+disp(stringU);
+disp(U);
+
+% compute the estimation
+estim = U * (U.');
+
+% Control results
+
+stringEstim = 'True Value / Value computed UU^T : ';
+disp(stringEstim);
+
+disp(A)
+disp(estim)
     
